e823f698e689f030057fb59bc0efc6db8f02c675
Takhshish_Bano <99491025+01takhshish@users.noreply.github.com>
2024-08-07 08:41:51 +0530

    Update ApiCall.js

--- Entertainment_App-Frontend/src/Features/ApiCall.js
+++ Entertainment_App-Frontend/src/Features/ApiCall.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "https://entertainment-app-backend-takhshish.onrender.com/";
+// const BASE_URL = "https://entertainment-app-backend-takhshish.onrender.com/";
+const BASE_URL = "https://entertainment-app-backend-3huo.onrender.com/";
 export default axios.create({
   baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" },
